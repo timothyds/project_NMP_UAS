5f10b10e06645358c19b4af76226b3dfe59dea49
Timothy Dewanto <dewantotims@gmail.com>
2024-01-04 00:51:33 +0700

    bottomNav

--- nmp_project_uas/app/src/main/java/com/nativepractice/nmp_project_uas/CreateFirstFragment.kt
+++ nmp_project_uas/app/src/main/java/com/nativepractice/nmp_project_uas/CreateFirstFragment.kt
@@ -17,7 +17,7 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class CreateFirstFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -38,15 +38,6 @@ class CreateFirstFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CreateFirstFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             CreateFirstFragment().apply {
--- nmp_project_uas/app/src/main/java/com/nativepractice/nmp_project_uas/MainActivity.kt
+++ nmp_project_uas/app/src/main/java/com/nativepractice/nmp_project_uas/MainActivity.kt
@@ -2,10 +2,39 @@ package com.nativepractice.nmp_project_uas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.nativepractice.nmp_project_uas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var fragments:ArrayList<Fragment> = arrayListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        fragments.add(HomeFragment())
+
+        binding.viewPager.adapter = MyAdapter(this,fragments)
+
+        binding.viewPager.registerOnPageChangeCallback(
+            object : ViewPager2.OnPageChangeCallback(){
+                override fun onPageSelected(position: Int) {
+                    binding.bottomNav.selectedItemId = binding.bottomNav.menu.getItem(position).itemId
+                }
+            }
+        )
+        binding.bottomNav.setOnItemSelectedListener {
+            binding.viewPager.currentItem = when(it.itemId){
+                R.id.itemHome ->0
+                //lanjutan fragment lain
+                //R.id.itemFollowing -> 1
+                //R.id.itemCreate -> 2
+                else -> 0
+            }
+            true
+        }
+
     }
 }
